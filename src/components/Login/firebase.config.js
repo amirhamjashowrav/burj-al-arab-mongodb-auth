5f608d8bbcdd53c604c56a82177be4d8a7e1453e
amirhamjashowrav <showrav022@gmail.com>
2021-03-27 08:40:12 +0600

    burj-al-arab-mongodb-done

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC5J-zKV1fDF8pNIokwT0VLvazZ6m6emyQ",
+    authDomain: "burj-al-arab-67710.firebaseapp.com",
+    projectId: "burj-al-arab-67710",
+    storageBucket: "burj-al-arab-67710.appspot.com",
+    messagingSenderId: "156121611849",
+    appId: "1:156121611849:web:92016f133a6ab66bfb0998"
+  };
+
+  export default firebaseConfig;
